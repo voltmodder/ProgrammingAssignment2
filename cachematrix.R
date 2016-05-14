65d41695df2a4190c3ca8c7550e87eb1afae7bfb
Voltmodder <D:\mail>
2016-05-14 14:16:56 +0600

    assestment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## function to pecial "matrix" object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setmean <- function(solve) s <<- solve
+  getmean <- function() s
+  list(set = set, get = get,
+       setmean = setsolve,
+       getmean = getsolve)
 }
 
 
-## Write a short comment describing this function
+## function tocompute the inverse of the special "matrix" 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
